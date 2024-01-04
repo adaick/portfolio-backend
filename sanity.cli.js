76fb3051e0efc25da0bf6ce776b964538065c434
Adarsh Mallick <adarshmallick@gmail.com>
2024-01-04 13:09:34 +0530

    added backend code for my portfolio

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '4n7wa6hb',
+    dataset: 'production'
+  }
+})
---
+++ schemas/index.js
@@ -0,0 +1,13 @@
+// importing schema types from their files
+
+import works from './works'
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './about'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+
+export const schemaTypes = [works, testimonials, brands, abouts, skills, workExperience, experiences, contact]
